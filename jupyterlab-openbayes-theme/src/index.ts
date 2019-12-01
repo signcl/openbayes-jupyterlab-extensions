70ce8cdd06be09ed89477ff1e2aade6ebe151def
HeroBcat <HeroBcat@users.noreply.github.com>
2019-12-02 00:34:08 +0800

    feat: openbayes theme

---
+++ jupyterlab-openbayes-theme/src/index.ts
@@ -0,0 +1,31 @@
+import {
+  JupyterFrontEnd, JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import {
+  IThemeManager
+} from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the jupyterlab-openbayes-theme extension.
+ */
+
+const namespace = 'jupyterlab-openbayes-theme'
+
+const extension: JupyterFrontEndPlugin<void> = {
+  id: namespace + ':plugin',
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    const style = namespace + '/index.css';
+    manager.register({
+      name: 'OpenBayes Theme',
+      isLight: false,
+      themeScrollbars: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  },
+  autoStart: true,
+};
+
+export default extension;
